e9bb24cd7f0342281ef8860c06d13b5bd9367d81
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-05-31 20:45:37 -0700

    stuffy stuff stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,16 @@ import './input.css';
 function App() {
   return (
     <div className={`min-h-screen`} data-theme='dark'>
-      <div className={`fixed top-0 w-screen py-2`}>
+      <div className={`fixed top-0 w-screen py-2 z-50`}>
         <div className={`w-4/6 mx-auto navbar rounded-lg bg-neutral px-4 py-4 text-neutral-content`}>
           <div className={`navbar-start`}>
             <button className={`btn btn-ghost text-3xl`}>Projectify</button>
           </div>
           <div className={`navbar-center`}>
             <ul className="menu menu-horizontal px-1">
-              <li className={`text-lg`}><a>Features</a></li>
-              <li className={`text-lg mx-2`}><a>Our Mission</a></li>
-              <li className={`text-lg`}><a>Contact</a></li>
+              <li className={`text-lg`}><a href={`#features`}>Features</a></li>
+              <li className={`text-lg mx-2`}><a href={`#mission`}>Our Mission</a></li>
+              <li className={`text-lg`}><a href={`#contact`}>Contact</a></li>
             </ul>
           </div>
           <div className={`navbar-end`}>
@@ -40,19 +40,81 @@ function App() {
           </div>
         </div>
       </div>
-      <div className={`min-h-screen`}>
+      <div className={``} id={`features`}>
         <div className={`w-11/12 rounded-xl p-10 mx-auto`}>
-          <h2 className={`text-6xl font-bold mb-10`}>Features</h2>
+          <h2 className={`text-6xl font-bold mb-10 text-center`}>Features</h2>
           <div className={`flex flex-row flex-wrap`}>
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card w-96 bg-base-100 shadow-xl mr-6 mb-6">
+              <figure><img src="https://t4.ftcdn.net/jpg/03/03/55/51/360_F_303555158_0h7YQZePL54n1UFE0Y5u4xqLSvHklqwc.jpg" alt="Shoes" /></figure>
               <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Buy Now</button>
-                </div>
+                <h2 className="card-title">Feature 1</h2>
+                <p>Explanation of feature 1</p>
               </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl mr-6 mb-6">
+              <figure><img src="https://t4.ftcdn.net/jpg/03/03/55/51/360_F_303555158_0h7YQZePL54n1UFE0Y5u4xqLSvHklqwc.jpg" alt="Shoes" /></figure>
+              <div className="card-body">
+              <h2 className="card-title">Feature 2</h2>
+                <p>Explanation of feature 2</p>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl mr-6 mb-6">
+              <figure><img src="https://t4.ftcdn.net/jpg/03/03/55/51/360_F_303555158_0h7YQZePL54n1UFE0Y5u4xqLSvHklqwc.jpg" alt="Shoes" /></figure>
+              <div className="card-body">
+              <h2 className="card-title">Feature 2</h2>
+                <p>Explanation of feature 2</p>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl mr-6 mb-6">
+              <figure><img src="https://t4.ftcdn.net/jpg/03/03/55/51/360_F_303555158_0h7YQZePL54n1UFE0Y5u4xqLSvHklqwc.jpg" alt="Shoes" /></figure>
+              <div className="card-body">
+              <h2 className="card-title">Feature 2</h2>
+                <p>Explanation of feature 2</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={``} id={`mission`}>
+        <div className={`w-11/12 p-10 rounded-xl mx-auto`}>
+          <h2 className={`text-6xl font-bold mb-10 text-center`}>Our Mission</h2>
+          <p className={`text-xl font-thin text-white leading-relaxed w-4/6 mx-auto tracking-wide text-center`}>At Projectify, we aim to support all JavaScript developers in their scaffolding, storing, hosting, and project management needs. Our extensive application tools allow teams to collaborate easily while acomplishing their project needs and checking statuses like a breeze.</p>
+        </div>
+      </div>
+      <div className={``} id={`contact`}>
+        <div className={`w-11/12 p-10 rounded-xl mx-auto`}>
+          <h2 className={`text-6xl font-bold mb-10 text-center`}>Contact Us</h2>
+          <div className={`p-5 rounded-xl shadow-xl w-2/6 mx-auto bg-neutral text-neutral-content`}>
+            <div id={`names`} className={`flex justify-between flex-row`}>
+              <label className="form-control w-[49%]">
+                <div className="label">
+                  <span className="label-text">First Name</span>
+                </div>
+                <input type="text" placeholder="Enter your first name" className="input input-bordered input-lg w-full" />
+              </label>
+              <label className="form-control w-[49%]">
+                <div className="label">
+                  <span className="label-text">Last Name</span>
+                  <span className="label-text-alt"><div className="badge badge-info gap-2 badge-outline">Optional</div></span>
+                </div>
+                <input type="text" placeholder="Enter your last name" className="input input-bordered input-lg w-full" />
+              </label>
+            </div>
+            <div className={`w-full`}>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text">Email</span>
+                </div>
+                <input type="text" placeholder="Enter your email" className="input input-bordered input-lg w-full" />
+              </label>
+            </div>
+            <div className={`w-full`}>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text">Message</span>
+                </div>
+                <textarea className="textarea textarea-bordered h-24 textarea-lg" placeholder="Enter your message"></textarea>
+              </label>
             </div>
           </div>
         </div>
